4757838ae1071ec82e335812f6e3158782ac283a
Wisdom <tgwisdom@una.edu>
2022-09-01 13:41:46 -0500

    added files and worked with ejs

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 
 const express = require('express')
+const path = require('node:path')
 const app = express()
 
+//setting view engine to ejs
+app.set("view engine", "ejs");
+
+let userName = "Joe";
+
 app.get('/', function (req, res) {
-  res.send('Hello World from Node/Express/Heroku')
+    //res.send('Hello ' + userName + ' from Node/Express/Heroku');
+    //res.sendFile(path.join(__dirname, "index.html" ));
+    //res.send(`Hello ${userName} from Heroku and Node`)
+     
+    res.render('index', 
+    { userNameEjs : userNameExpress
+    }
+    );
 })
 
 app.listen(process.env.PORT || 3000,

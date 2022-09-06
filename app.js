a5729fe3f30457657d78729411c698b1dfe6a3b9
tgwisdom <tgwisdom@una.edu>
2022-09-06 00:52:47 -0500

    worked to catch up to studious-robot progress

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
 const express = require('express')
 const path = require('node:path')
+const bodyParser = require('body-parser')
 const app = express()
 
 //setting view engine to ejs
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
 app.set("view engine", "ejs");
 
-let userName = "Joe";
+
+
+
+let userName = "Troy";
+let thatData ="";
 
 app.get('/', function (req, res) {
     //res.send('Hello ' + userName + ' from Node/Express/Heroku');
@@ -14,10 +21,17 @@ app.get('/', function (req, res) {
     //res.send(`Hello ${userName} from Heroku and Node`)
      
     res.render('index', 
-    { userNameEjs : userNameExpress
+    { 
+        userName: userName
     }
     );
 })
+    
+    app.post("/savetoNode", (req, res) => {
+        console.log(req.body);
+        console.group(req.body.userName);
+        res.render('index', { userName: req.body.userName });
+    })
 
 app.listen(process.env.PORT || 3000,
   () => console.log("Server is running..."));
